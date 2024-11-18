c2bdf8f8c60e6970b84075c741ea660a3c2fb16f
Variety154 <varvarakit15042103@gmail.com>
2024-11-18 16:30:24 +0300

    исправления

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = DarkStatusBarViewController()
         window?.makeKeyAndVisible()
  
         return true
